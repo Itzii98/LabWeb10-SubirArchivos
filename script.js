9089fb41ef9541ce009cf9782c39575be436f99c
LabP3E8-07 <LabP3E8-07@1-20-03-E008-07>
2023-08-16 17:45:40 -0500

    Mejoras incorporadas: validación extensión tamaño

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
 const fileInput = document.getElementById('fileInput');
-
 const fileInfo = document.getElementById('fileInfo');
-
 const preview = document.getElementById('preview');
 
-
 fileInput.addEventListener('change', function(event) {
-
   const selectedFile = event.target.files[0];
+  
+  const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+  const maxSize = 5 * 1024 * 1024; // 5 MB in bytes
 
+  const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
 
-  fileInfo.innerHTML = `
+  if (allowedExtensions.includes(fileExtension) && selectedFile.size <= maxSize) {
+    fileInfo.innerHTML = `
+      Nombre del archivo: ${selectedFile.name}<br>
+      Tipo MIME: ${selectedFile.type}<br>
+      Tamaño: ${selectedFile.size} bytes
+    `;
 
-    Nombre del archivo: ${selectedFile.name}<br>
-
-    Tipo MIME: ${selectedFile.type}<br>
-
-    Tamaño: ${selectedFile.size} bytes
-
-  `;
-
-
-  if (selectedFile.type.startsWith('image/')) {
-
-    preview.style.display = 'block';
-
-    const reader = new FileReader();
-
-    reader.onload = function(e) {
-
-      preview.src = e.target.result;
-
-    };
-
-    reader.readAsDataURL(selectedFile);
-
+    if (selectedFile.type.startsWith('image/')) {
+      preview.style.display = 'block';
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+      };
+      reader.readAsDataURL(selectedFile);
+    } else {
+      preview.style.display = 'none';
+    }
   } else {
-
+    fileInfo.innerHTML = 'Archivo no válido. Por favor, selecciona un archivo .jpg, .png o .gif de hasta 5 MB.';
     preview.style.display = 'none';
-
+    fileInput.value = ''; // Clear the input
   }
-
 });
